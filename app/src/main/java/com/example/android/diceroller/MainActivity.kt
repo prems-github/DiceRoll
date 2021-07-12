1b42df17a27cb2b0d9ee3b89cae2a9c514d42f05
Prem <premdandy@gmail.com>
2021-07-12 23:06:34 +0530

    view binding - button click

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.android.diceroller
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rollButton.setOnClickListener {
+            Toast.makeText(this, "Lets Roll", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
