f2c19d883c736fd2fe00a09ece1d8f6544847611
Prem <premdandy@gmail.com>
2021-07-13 06:47:06 +0530

    roll dice function

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.android.diceroller
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.rollButton.setOnClickListener {
-            Toast.makeText(this, "Lets Roll", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
 
+    }
+
+    private fun rollDice() {
+
+        val randomInt = Random.nextInt(6) + 1
+        binding.rollText.text = randomInt.toString()
     }
 }
